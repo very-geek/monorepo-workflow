f0358b2b8b3168b56d803abed989db3e42b7aef6
Albert Yu <nightire@users.noreply.github.com>
2020-03-02 15:06:10 +0800

    release new version
    - foo with minor update
    - bar with patch update

--- packages/bar/index.js
+++ packages/bar/index.js
@@ -1,7 +1,5 @@
 import foo from '@very-geek/foo';
 
 module.exports = function bar() {
-  // prepend version info from foo package to the output
-  // 为输出结果追加 foo 的版本信息
   return `${foo()} with @very-geek/bar@1.x`;
 };
--- packages/foo/index.js
+++ packages/foo/index.js
@@ -1,5 +1,3 @@
 module.exports = function foo() {
-  // provide package version information
-  // another thing to comment
   return '@very-geek/foo@1.x';
 };
